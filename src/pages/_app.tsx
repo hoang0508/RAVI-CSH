17544cd88a9e7158216837e9b2536e8147c61ab0
hoang0508 <nguyenhuyhoang05082001@gmail.com>
2023-04-21 08:53:12 +0700

    setup project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.scss";
+import type { AppProps } from "next/app";
+import { Lexend } from "next/font/google";
+const lexend = Lexend({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${lexend.className}`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
