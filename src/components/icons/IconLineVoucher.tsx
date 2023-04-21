08c2b4cba9d29b82823ff28ec40b22af3b69d88b
hoang0508 <nguyenhuyhoang05082001@gmail.com>
2023-04-21 09:11:08 +0700

    add folder icons

---
+++ src/components/icons/IconLineVoucher.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const IconLineVoucher = () => {
+  return (
+    <>
+      <svg
+        width="364"
+        height="1"
+        viewBox="0 0 364 1"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <line
+          x1="0.5"
+          y1="0.5"
+          x2="363.5"
+          y2="0.500032"
+          stroke="#D2C4B4"
+          stroke-linecap="round"
+          stroke-dasharray="10 10"
+        />
+      </svg>
+    </>
+  );
+};
+
+export default IconLineVoucher;
